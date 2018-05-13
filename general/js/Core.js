f3209010a4cde85910f3664d1157aeb7da74ea3b
Andre Anaya <andre.anaya@gmail.com>
2018-05-13 01:23:51 +0100

    no message

--- general/js/Core.js
+++ general/js/Core.js
@@ -1,15 +1,15 @@
 import 'boilerplate/general/scss/index.scss';
 
-import {broadcast} from 'boilerplate/general/js/EventSystem.js';
-import 'boilerplate/general/js/Factory.js';
+import 'boilerplate/general/js/EventSystem.js';
 import 'boilerplate/general/js/HistoryController.js';
-import 'boilerplate/general/js/GlobalEvents.js';
 import 'boilerplate/general/js/Gesture.js';
+import 'boilerplate/general/js/GlobalEvents.js';
 import 'boilerplate/general/js/ScrollController.js';
+import 'boilerplate/general/js/Factory.js';
 
 export function init() {
 	setTimeout(function() {
-		broadcast(new CustomEvent('app:init'));
+		document.body.broadcast(new CustomEvent('app:init'));
 		document.body.emit(new CustomEvent('app:show'));
 	}, 100);
 }
